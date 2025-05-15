456be369a273fc9c0d5449b899cac99ceb4ecfbe
wolfohgiraffers <wolf.ohgiraffers@gmail.com>
2025-05-15 10:49:35 +0900

    bug fixed

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
+    <!-- <input type="text" v-model="test" placeholder=".shop주소 도메인을 입력하세요"> -->
     <button v-on:click="move">무브무브</button>
   </div>
 </template>
 
 <script setup>
+  // import {ref} from 'vue';
+
+  // const test = ref('');
+
   function move() {
+    // location.href='https://' + test + '.shop';
     location.href='https://naver.com';
   }
 </script>
