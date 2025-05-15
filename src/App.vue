1d93b7804c2bd7c6a9b54e9ad3fc14131a047a4e
wolfohgiraffers <wolf.ohgiraffers@gmail.com>
2025-05-15 10:09:47 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <button v-on:click="move">무브무브</button>
+  </div>
+</template>
+
+<script setup>
+  function move() {
+    location.href='https://naver.com';
+  }
+</script>
